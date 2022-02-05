745e9773b496d5da424b0079310c4cc44436fedf
Andrzej Prochyra <andrzej@prochyra.name>
2022-02-05 19:21:17 +0000

    Added isTurned state to determine content

--- src/main/java/io/prochyra/flashcardapp/Card.java
+++ src/main/java/io/prochyra/flashcardapp/Card.java
@@ -4,6 +4,7 @@ public class Card {
 
   private final String definition;
   private String concept;
+  private boolean isTurned;
 
   public Card(String concept, String definition) {
     this.concept = concept;
@@ -11,10 +12,13 @@ public class Card {
   }
 
   public String content() {
+    if (isTurned) {
+      return definition;
+    }
     return concept;
   }
 
   public void turn() {
-    concept = definition;
+    isTurned = true;
   }
 }
