1392256deb77ccffcd189d557a16874eff1616b8
Andrzej Prochyra <andrzej@prochyra.name>
2022-02-18 20:52:26 +0000

    Remove unnecessary field

--- src/main/java/io/prochyra/flashcardapp/domain/StudySession.java
+++ src/main/java/io/prochyra/flashcardapp/domain/StudySession.java
@@ -3,11 +3,9 @@ package io.prochyra.flashcardapp.domain;
 import java.util.Iterator;
 
 public class StudySession {
-    private final Deck deck;
     private final Iterator<Card> cardIterator;
 
     public StudySession(Deck deck, int cardCount) {
-        this.deck = deck;
         cardIterator = deck.iterator();
     }
 
