3bfb6ff8e5c4255514560dc01526cd3262be2304
Andrzej Prochyra <andrzej@prochyra.name>
2022-03-18 18:32:16 +0000

    Deck tests set card confidence first

--- src/test/java/io/prochyra/flashcardapp/domain/DeckTest.java
+++ src/test/java/io/prochyra/flashcardapp/domain/DeckTest.java
@@ -7,50 +7,47 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DeckTest {
-    
+
     @Test
     void givesACountOfHighConfidenceCards() {
         Card unknownConfidenceCard1 = new Card("concept", "definition");
         Card unknownConfidenceCard2 = new Card("concept", "definition");
-        Card highConfidenceCard = new Card("concept", "definition");
-        highConfidenceCard.flip();
-        highConfidenceCard.recordConfidence(Confidence.HIGH);
+        Card highConfidenceCard = new Card("concept", "definition", Confidence.HIGH);
         Deck deck = new Deck(List.of(unknownConfidenceCard1, unknownConfidenceCard2, highConfidenceCard));
 
-        assertThat(deck.highConfidenceCount()).isOne();
+        assertThat(deck.highConfidenceCount())
+                .isOne();
     }
 
     @Test
     void givesTotalCountOfCards() {
         List<Card> threeCards = List.of(
-                new Card("a", "a"), 
-                new Card("b", "b"), 
+                new Card("a", "a"),
+                new Card("b", "b"),
                 new Card("c", "c")
         );
-        
+
         Deck deck = new Deck(threeCards);
-        
+
         assertThat(deck.totalCardCount())
                 .isEqualTo(3);
     }
 
     @Test
     void givesCountOfMediumConfidenceCards() {
-        Card mediumConfidenceCard = new Card("x", "x");
-        mediumConfidenceCard.flip();
-        mediumConfidenceCard.recordConfidence(Confidence.MEDIUM);
+        Card mediumConfidenceCard = new Card("x", "x", Confidence.MEDIUM);
         Deck deck = new Deck(List.of(mediumConfidenceCard));
-        
-        assertThat(deck.mediumConfidenceCount()).isOne();
+
+        assertThat(deck.mediumConfidenceCount())
+                .isOne();
     }
 
     @Test
     void givesCountOfLowConfidenceCards() {
-        Card lowConfidenceCard = new Card("x", "x");
-        lowConfidenceCard.flip();
-        lowConfidenceCard.recordConfidence(Confidence.LOW);
+        Card lowConfidenceCard = new Card("x", "x", Confidence.LOW);
         Deck deck = new Deck(List.of(lowConfidenceCard));
 
-        assertThat(deck.lowConfidenceCount()).isOne();
+        assertThat(deck.lowConfidenceCount())
+                .isOne();
     }
 }
