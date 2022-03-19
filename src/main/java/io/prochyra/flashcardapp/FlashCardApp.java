bce4dedd5f4105d0cec2f28af875fa329dbd98e9
Andrzej Prochyra <andrzej@prochyra.name>
2022-03-19 16:32:25 +0000

    FlashCardApp uses Card repository

--- src/main/java/io/prochyra/flashcardapp/FlashCardApp.java
+++ src/main/java/io/prochyra/flashcardapp/FlashCardApp.java
@@ -1,20 +1,20 @@
 package io.prochyra.flashcardapp;
 
 import io.prochyra.flashcardapp.adapter.in.console.ConsoleAdapter;
+import io.prochyra.flashcardapp.application.port.InMemoryCardRepository;
 import io.prochyra.flashcardapp.domain.Card;
 import io.prochyra.flashcardapp.domain.Deck;
 
-import java.util.List;
-
 public class FlashCardApp {
+    
     public static void main(String[] args) {
-        // put this into the (InMemory)CardRepository
+        InMemoryCardRepository cardRepository = new InMemoryCardRepository();
         Card card1 = new Card("concept1", "definition1");
         Card card2 = new Card("concept2", "definition2");
-        List<Card> cards = List.of(card1, card2);
+        cardRepository.save(card1);
+        cardRepository.save(card2);
 
-        // create this using the CardRepository and not the list
-        Deck deck = new Deck(cards);
+        Deck deck = new Deck(cardRepository.findAll());
 
         ConsoleAdapter consoleAdapter = new ConsoleAdapter(deck);
         consoleAdapter.start();
