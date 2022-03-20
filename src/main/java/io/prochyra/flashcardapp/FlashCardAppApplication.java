e9306060c249183dee5d16f32e51d82f2615b64d
Andrzej Prochyra <andrzej@prochyra.name>
2022-03-20 21:40:56 +0000

    Four initial stub cards saved to card repository on startup

--- src/main/java/io/prochyra/flashcardapp/FlashCardAppApplication.java
+++ src/main/java/io/prochyra/flashcardapp/FlashCardAppApplication.java
@@ -2,6 +2,8 @@ package io.prochyra.flashcardapp;
 
 import io.prochyra.flashcardapp.application.port.CardRepository;
 import io.prochyra.flashcardapp.application.port.InMemoryCardRepository;
+import io.prochyra.flashcardapp.domain.Card;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,5 +18,15 @@ public class FlashCardAppApplication {
     @Bean
     CardRepository cardRepository() {
         return new InMemoryCardRepository();
+    }
+
+    @Bean
+    ApplicationRunner dataLoader(CardRepository cardRepository) {
+        return args -> {
+            cardRepository.save(new Card("concept1", "definition1"));
+            cardRepository.save(new Card("concept2", "definition2"));
+            cardRepository.save(new Card("concept3", "definition3"));
+            cardRepository.save(new Card("concept4", "definition4"));
+        };
     }
 }
