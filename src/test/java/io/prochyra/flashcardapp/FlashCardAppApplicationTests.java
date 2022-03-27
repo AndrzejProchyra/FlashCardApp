89bfa7cd659e5cc84e62407c61b739b4b6adceeb
Andrzej Prochyra <andrzej@prochyra.name>
2022-03-27 21:13:49 +0100

    Add Testcontainers containerized PostgreSQL to application tests

--- src/test/java/io/prochyra/flashcardapp/FlashCardAppApplicationTests.java
+++ src/test/java/io/prochyra/flashcardapp/FlashCardAppApplicationTests.java
@@ -3,11 +3,29 @@ package io.prochyra.flashcardapp;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Tag("integration")
 @SpringBootTest
+@Testcontainers
 class FlashCardAppApplicationTests {
 
+  @Container
+  static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
+          .withPassword("inmemory")
+          .withUsername("inmemory");
+
+  @DynamicPropertySource
+  static void postgresqlProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+    registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+  }
+  
   @Test
   void contextLoads() {
   }
