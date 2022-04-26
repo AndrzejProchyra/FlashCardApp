ab425e974b10477adc0e7e14211940bb6441be5f
Andrzej Prochyra <andrzej@prochyra.name>
2022-04-26 18:24:11 +0100

    Refactor DB tests to use Testcontainers setup base class

--- src/test/java/io/prochyra/flashcardapp/adapter/out/jpa/CardDboTest.java
+++ src/test/java/io/prochyra/flashcardapp/adapter/out/jpa/CardDboTest.java
@@ -1,40 +1,15 @@
 package io.prochyra.flashcardapp.adapter.out.jpa;
 
 import io.prochyra.flashcardapp.domain.Confidence;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
-@Testcontainers
-@Tag("integration")
-class CardDboTest {
-
-    @Container
-    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest")
-            .withPassword("inmemory")
-            .withUsername("inmemory");
+class CardDboTest extends PostgresTestcontainerBase {
 
     @Autowired
     CardJpaRepository cardJpaRepository;
-
-    @DynamicPropertySource
-    static void postgresqlProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-    }
 
     @Test
     void storedCardDboIsRetrievedFromJpaRepositoryWithIdAssigned() {
